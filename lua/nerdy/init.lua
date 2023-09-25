92308470e84baeb5244632bf7efa577a8ce2326d
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-09-25 07:37:54 +0530

    fix: insert raw icon string by default, fixes #1

--- lua/nerdy/init.lua
+++ lua/nerdy/init.lua
@@ -14,14 +14,14 @@ nerdy.list = function()
     if #icon_list > 0 then
         vim.ui.select(icon_list, { prompt = 'Select an icon:' }, function(item, _)
             if item then
-                local string = "'" .. vim.split(item, ' : ')[2] .. " '"
+                local icon = vim.split(item, ' : ')[2]
                 local cursor_position = vim.api.nvim_win_get_cursor(0)
-                local line = cursor_position[1]
+                local row = cursor_position[1]
                 local column = cursor_position[2]
                 -- insert icon after cursor
-                vim.api.nvim_buf_set_text(0, line - 1, column, line - 1, column, { string })
+                vim.api.nvim_buf_set_text(0, row - 1, column, row - 1, column, { icon })
                 -- move cursor to the end of the inserted icon
-                vim.api.nvim_win_set_cursor(0, { line, column + #string })
+                vim.api.nvim_win_set_cursor(0, { row, column + #icon })
             end
         end)
     end
