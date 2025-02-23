354757e0f8d3b0ad35eca270048b8695ab44daac
Stefan Zuefeldt <stefan.zuefeldt@gmail.com>
2025-02-23 00:02:13 -0600

    fix: preserve cursor position
    Cursor position in insert mode, when at the end of a line, was not being
    preserved

--- lua/nerdy/init.lua
+++ lua/nerdy/init.lua
@@ -3,6 +3,7 @@ local nerdy = {}
 nerdy.list = function()
     local icon_list = require('nerdy.icons')
     local initial_mode = vim.api.nvim_get_mode().mode
+    local cursor_position = vim.api.nvim_win_get_cursor(0)
 
     vim.ui.select(icon_list, {
         prompt = 'Nerdy Icons',
@@ -14,11 +15,12 @@ nerdy.list = function()
         if item ~= nil then
             local is_insert_mode = (initial_mode == 'i')
 
-            vim.api.nvim_put({ item.char }, 'c', not is_insert_mode, true)
-
             if is_insert_mode then
                 vim.cmd('startinsert')
             end
+            vim.api.nvim_win_set_cursor(0, cursor_position)
+
+            vim.api.nvim_put({ item.char }, 'c', not is_insert_mode, true)
         end
     end)
 end
