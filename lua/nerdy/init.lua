3b82c6275cc183c5ec960ae393529613527ab509
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-06-10 09:34:18 +0530

    feat: add recent icons tracking to main UI functions
    - Add recent icons tracking to nerdy.list() function
    - Create nerdy.list_recent() function for recent-only selection
    - Integrate with nerdy.recent module for persistence
    - Update setup() function to use centralized config module
    - Remove redundant wrapper functions for cleaner API
    - Maintain backward compatibility for existing functionality

--- lua/nerdy/init.lua
+++ lua/nerdy/init.lua
@@ -1,4 +1,5 @@
 local nerdy = {}
+local recent = require('nerdy.recent')
 
 nerdy.list = function()
     local icon_list = require('nerdy.icons')
@@ -13,6 +14,35 @@ nerdy.list = function()
         end,
     }, function(item, _)
         if item ~= nil then
+            recent.add_to_recent(item)
+            if initial_mode == 'i' then
+                vim.cmd('startinsert')
+            end
+            vim.api.nvim_win_set_cursor(0, cursor_position)
+            vim.api.nvim_put({ item.char }, 'c', false, true)
+        end
+    end)
+end
+
+nerdy.list_recent = function()
+    local recent_icons = recent.load_recent_icons()
+    if #recent_icons == 0 then
+        vim.notify('No recent icons found', vim.log.levels.INFO)
+        return
+    end
+
+    local initial_mode = vim.api.nvim_get_mode().mode
+    local cursor_position = vim.api.nvim_win_get_cursor(0)
+
+    vim.ui.select(recent_icons, {
+        prompt = 'Recent Nerdy Icons',
+        format_item = function(item)
+            local item_str = string.format('%s (%s) : %s', item.name, item.code, item.char)
+            return item_str
+        end,
+    }, function(item, _)
+        if item ~= nil then
+            recent.add_to_recent(item)
             if initial_mode == 'i' then
                 vim.cmd('startinsert')
             end
@@ -34,6 +64,11 @@ nerdy.get = function(name)
     end
     vim.notify('Icon not found: ' .. name, vim.log.levels.WARN)
     return ''
+end
+
+nerdy.setup = function(opts)
+    local config = require('nerdy.config')
+    config.setup(opts)
 end
 
 return nerdy
