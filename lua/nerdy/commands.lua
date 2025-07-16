104f7de718538bc55e5effc2dde606ede7ba9553
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-07-17 03:33:55 +0530

    feat!: remove legacy commands

--- lua/nerdy/commands.lua
+++ lua/nerdy/commands.lua
@@ -77,24 +77,10 @@ end
 
 ---Setup Nerdy commands
 function M.setup()
-    if config.use_new_command then
-        add_nerdy_command()
-        if config.add_default_keybindings then
-            vim.api.nvim_set_keymap('n', '<leader>in', ':Nerdy list<CR>', { noremap = true, silent = true })
-            vim.api.nvim_set_keymap('n', '<leader>iN', ':Nerdy recents<CR>', { noremap = true, silent = true })
-        end
-    else
-        vim.notify(
-            'Legacy Nerdy commands are deprecated and will be removed in a future version.\n'
-                .. 'Please switch to the new `:Nerdy` command by adding `use_new_command = true` in your config.\n'
-                .. 'More info: https://github.com/2kabhishek/nerdy.nvim/pull/14',
-            vim.log.levels.WARN
-        )
-        legacy.add_legacy_commands()
-        if config.add_default_keybindings then
-            vim.api.nvim_set_keymap('n', '<leader>in', ':Nerdy<CR>', { noremap = true, silent = true })
-            vim.api.nvim_set_keymap('n', '<leader>iN', ':NerdyRecents<CR>', { noremap = true, silent = true })
-        end
+    add_nerdy_command()
+    if config.add_default_keybindings then
+        vim.api.nvim_set_keymap('n', '<leader>in', ':Nerdy list<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<leader>iN', ':Nerdy recents<CR>', { noremap = true, silent = true })
     end
 end
 
--- lua/nerdy/config.lua
+++ lua/nerdy/config.lua
@@ -3,11 +3,9 @@ local M = {}
 ---@class nerdy.config
 ---@field max_recents integer : Max number of recent icons to keep
 ---@field add_default_keybindings boolean : Whether to add default keybindings
----@field use_new_command boolean : Whether to use new Nerdy command
 M.config = {
     max_recents = 30,
     add_default_keybindings = true,
-    use_new_command = false,
 }
 
 M.setup = function(opts)
