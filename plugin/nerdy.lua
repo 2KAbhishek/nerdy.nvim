7e21c7c3a8ad0b8a47819a47d54e51466fe5f69e
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-07-17 01:16:56 +0530

    feat!: get rid of old setup logic

--- plugin/nerdy.lua
+++
@@ -1,8 +0,0 @@
-if vim.g.loaded_nerdy then
-    return
-end
-
-vim.g.loaded_nerdy = true
-
-vim.api.nvim_create_user_command('Nerdy', 'lua require("nerdy").list()', {})
-vim.api.nvim_create_user_command('NerdyRecents', 'lua require("nerdy").list_recent()', {})
