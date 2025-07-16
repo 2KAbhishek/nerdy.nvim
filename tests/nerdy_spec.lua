95000780575458b494be5c4883ce63f7e35f7cf5
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-07-17 03:26:06 +0530

    test: updated tests to reflect new module structure

--- tests/nerdy_spec.lua
+++ tests/nerdy_spec.lua
@@ -1,71 +1,69 @@
 local nerdy = require('nerdy')
+local fetcher = require('nerdy.fetcher')
 
 describe('nerdy', function()
     before_each(function()
-        local recent = require('nerdy.recent')
-        recent.save_recent_icons({})
+        local recents = require('nerdy.recents')
+        recents.save_recent_icons({})
     end)
 
     after_each(function()
-        local recent = require('nerdy.recent')
-        recent.save_recent_icons({})
+        local recents = require('nerdy.recents')
+        recents.save_recent_icons({})
     end)
 
     it('has the right functions defined', function()
-        assert.is_function(nerdy.list)
-        assert.is_function(nerdy.list_recent)
-        assert.is_function(nerdy.get)
         assert.is_function(nerdy.setup)
     end)
 
     describe('get function', function()
         it('returns the correct character for a valid icon name', function()
-            local char = nerdy.get('cod-account')
+            local char = fetcher.get('cod-account')
             assert(char == '', 'Expected cod-account icon character to be ""')
         end)
 
         it('returns the correct character for another valid icon name', function()
-            local char = nerdy.get('cod-add')
+            local char = fetcher.get('cod-add')
             assert(char == '', 'Expected cod-add icon character to be ""')
         end)
 
         it('returns empty string for non-existent icon', function()
-            local char = nerdy.get('non-existent-icon')
+            local char = fetcher.get('non-existent-icon')
             assert(char == '', 'Expected empty string for non-existent icon')
         end)
 
         it('returns empty string for nil input', function()
-            local char = nerdy.get(nil)
+            local char = fetcher.get(nil)
             assert(char == '', 'Expected empty string for nil input')
         end)
 
         it('returns empty string for empty string input', function()
-            local char = nerdy.get('')
+            local char = fetcher.get('')
             assert(char == '', 'Expected empty string for empty string input')
         end)
     end)
 
     describe('list function', function()
         it('exists and is callable', function()
-            assert.is_function(nerdy.list)
+            assert.is_function(fetcher.list)
         end)
 
         it('calls vim.ui.select with icon list', function()
             local original_ui_select = vim.ui.select
             local called_with_items = nil
             local called_with_opts = nil
-            
+
             vim.ui.select = function(items, opts, callback)
                 called_with_items = items
                 called_with_opts = opts
                 -- Don't call callback to avoid UI interaction
             end
-            
-            nerdy.list()
-            
+
+            fetcher.list()
+
             -- Restore original function
             vim.ui.select = original_ui_select
-            
+
             assert.is_not_nil(called_with_items)
             assert.is_table(called_with_items)
             assert(#called_with_items > 0, 'Expected non-empty icon list')
@@ -76,16 +74,16 @@ describe('nerdy', function()
         it('formats items correctly', function()
             local original_ui_select = vim.ui.select
             local format_function = nil
-            
+
             vim.ui.select = function(items, opts, callback)
                 format_function = opts.format_item
             end
-            
-            nerdy.list()
+
+            fetcher.list()
             vim.ui.select = original_ui_select
-            
+
             assert.is_function(format_function)
-            
+
             local test_item = { name = 'test-icon', code = 'e123', char = '' }
             local formatted = format_function(test_item)
             assert.is_string(formatted)
@@ -96,71 +94,71 @@ describe('nerdy', function()
         it('adds selected icon to recent when callback is called', function()
             local original_ui_select = vim.ui.select
             local selection_callback = nil
-            
+
             vim.ui.select = function(items, opts, callback)
                 selection_callback = callback
             end
-            
-            nerdy.list()
+
+            fetcher.list()
             vim.ui.select = original_ui_select
-            
+
             assert.is_function(selection_callback)
-            
+
             -- Test that selecting an icon adds it to recent
-            local recent_utils = require('nerdy.recent')
+            local recent_utils = require('nerdy.recents')
             local recent_before = recent_utils.load_recent_icons()
             assert.are.equal(0, #recent_before)
-            
+
             local test_icon = { name = 'cod-account', code = 'eb99', char = '' }
             selection_callback(test_icon, 1)
-            
+
             local recent_after = recent_utils.load_recent_icons()
             assert.are.equal(1, #recent_after)
             assert.are.equal('cod-account', recent_after[1].name)
         end)
     end)
 
-    describe('list_recent function', function()
+    describe('list_recents function', function()
         it('exists and is callable', function()
-            assert.is_function(nerdy.list_recent)
+            assert.is_function(fetcher.list_recents)
         end)
 
         it('shows info message when no recent icons exist', function()
             local original_notify = vim.notify
             local notification_message = nil
             local notification_level = nil
-            
+
             vim.notify = function(message, level)
                 notification_message = message
                 notification_level = level
             end
-            
-            nerdy.list_recent()
-            
+
+            fetcher.list_recents()
+
             vim.notify = original_notify
-            
+
             assert.are.equal('No recent icons found', notification_message)
             assert.are.equal(vim.log.levels.INFO, notification_level)
         end)
 
         it('calls vim.ui.select when recent icons exist', function()
             -- Add a recent icon first
-            local recent_utils = require('nerdy.recent')
+            local recent_utils = require('nerdy.recents')
             local icon_list = require('nerdy.icons')
             recent_utils.add_to_recent(icon_list[1])
-            
+
             local original_ui_select = vim.ui.select
             local called_with_items = nil
             local called_with_opts = nil
-            
+
             vim.ui.select = function(items, opts, callback)
                 called_with_items = items
                 called_with_opts = opts
             end
-            
-            nerdy.list_recent()
+
+            fetcher.list_recents()
             vim.ui.select = original_ui_select
-            
+
             assert.is_not_nil(called_with_items)
             assert.is_table(called_with_items)
             assert.are.equal(1, #called_with_items)
@@ -170,27 +168,27 @@ describe('nerdy', function()
 
         it('updates recent list when selection is made', function()
             -- Add two recent icons
-            local recent_utils = require('nerdy.recent')
+            local recent_utils = require('nerdy.recents')
             local icon_list = require('nerdy.icons')
             recent_utils.add_to_recent(icon_list[1])
             recent_utils.add_to_recent(icon_list[2])
-            
+
             local original_ui_select = vim.ui.select
             local selection_callback = nil
-            
+
             vim.ui.select = function(items, opts, callback)
                 selection_callback = callback
             end
-            
-            nerdy.list_recent()
+
+            fetcher.list_recents()
             vim.ui.select = original_ui_select
-            
+
             -- Select the second icon (should move it to front)
             local recent_before = recent_utils.load_recent_icons()
             assert.are.equal(2, #recent_before)
-            
+
             selection_callback(recent_before[2], 2)
-            
+
             local recent_after = recent_utils.load_recent_icons()
             assert.are.equal(2, #recent_after)
             assert.are.equal(recent_before[2].name, recent_after[1].name)
@@ -199,21 +197,21 @@ describe('nerdy', function()
 
     describe('recent icons functionality', function()
         it('starts with empty recent icons list', function()
-            local recent_utils = require('nerdy.recent')
-            local recent = recent_utils.load_recent_icons()
-            assert.are.same({}, recent)
+            local recent_utils = require('nerdy.recents')
+            local recents = recent_utils.load_recent_icons()
+            assert.are.same({}, recents)
         end)
 
         it('does not add icon to recent when using get function', function()
-            nerdy.get('cod-account')
-            local recent_utils = require('nerdy.recent')
-            local recent = recent_utils.load_recent_icons()
-            assert.are.equal(0, #recent)
+            fetcher.get('cod-account')
+            local recent_utils = require('nerdy.recents')
+            local recents = recent_utils.load_recent_icons()
+            assert.are.equal(0, #recents)
         end)
 
         it('clears recent icons', function()
             -- Manually add icons to test clearing
-            local recent_utils = require('nerdy.recent')
+            local recent_utils = require('nerdy.recents')
             local icon_list = require('nerdy.icons')
             recent_utils.add_to_recent(icon_list[1])
             recent_utils.add_to_recent(icon_list[2])
@@ -229,7 +227,7 @@ describe('nerdy', function()
 
         it('persists recent icons across sessions', function()
             -- Manually add icon to test persistence
-            local recent_utils = require('nerdy.recent')
+            local recent_utils = require('nerdy.recents')
             local icon_list = require('nerdy.icons')
             recent_utils.add_to_recent(icon_list[1])
 
@@ -248,22 +246,22 @@ describe('nerdy', function()
     end)
 
     describe('setup function', function()
-        it('configures max_recent setting', function()
-            nerdy.setup({ max_recent = 5 })
+        it('configures max_recents setting', function()
+            nerdy.setup({ max_recents = 5 })
 
-            -- Manually add icons to test max_recent limit
-            local recent_utils = require('nerdy.recent')
+            -- Manually add icons to test max_recents limit
+            local recent_utils = require('nerdy.recents')
             local icon_list = require('nerdy.icons')
             for i = 1, 7 do
                 recent_utils.add_to_recent(icon_list[i])
             end
 
-            local recent = recent_utils.load_recent_icons()
-            assert.are.equal(5, #recent)
+            local recents = recent_utils.load_recent_icons()
+            assert.are.equal(5, #recents)
         end)
 
         it('merges configuration options', function()
-            nerdy.setup({ max_recent = 15 })
+            nerdy.setup({ max_recents = 15 })
 
             -- Test that configuration is properly merged
             assert.is_function(nerdy.setup)
