fed46d334c04a7df551a78f914689a52a2ea6c1e
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-10-16 10:18:13 +0530

    feat: add multi select support

--- lua/nerdy/fetcher.lua
+++ lua/nerdy/fetcher.lua
@@ -2,12 +2,55 @@ local fetcher = {}
 local recents = require('nerdy.recents')
 local config_module = require('nerdy.config')
 
-local function insert_icon_from_list(icon_list, list_titie)
+local function multi_select(icon_list, list_title, snacks)
+    local initial_mode = vim.api.nvim_get_mode().mode
+    local cursor_position = vim.api.nvim_win_get_cursor(0)
+
+    local items = {}
+    for _, icon in ipairs(icon_list) do
+        items[#items + 1] = {
+            text = string.format('%s (%s) : %s', icon.name, icon.code, icon.char),
+            icon = icon
+        }
+    end
+
+    snacks.picker.pick({
+        items = items,
+        format = 'text',
+        layout = {
+            preset = "select"
+        },
+        title = list_title,
+        confirm = function(picker, item)
+            local selected = picker:selected({ fallback = true })
+            picker:close()
+
+            if #selected > 0 then
+                local chars = {}
+                for _, selected_item in ipairs(selected) do
+                    recents.add_to_recent(selected_item.icon)
+                    table.insert(chars, selected_item.icon.char)
+                end
+                if config_module.config.copy_to_clipboard then
+                    vim.fn.setreg('+', table.concat(chars, ''))
+                    return
+                end
+                if initial_mode == 'i' then
+                    vim.cmd('startinsert')
+                end
+                vim.api.nvim_win_set_cursor(0, cursor_position)
+                vim.api.nvim_put({ table.concat(chars, ' ') }, 'c', false, true)
+            end
+        end
+    })
+end
+
+local function single_select(icon_list, list_title)
     local initial_mode = vim.api.nvim_get_mode().mode
     local cursor_position = vim.api.nvim_win_get_cursor(0)
 
     vim.ui.select(icon_list, {
-        prompt = list_titie,
+        prompt = list_title,
         format_item = function(item)
             local item_str = string.format('%s (%s) : %s', item.name, item.code, item.char)
             return item_str
@@ -26,6 +69,15 @@ local function insert_icon_from_list(icon_list, list_titie)
             vim.api.nvim_put({ item.char }, 'c', false, true)
         end
     end)
+end
+
+local function insert_icon_from_list(icon_list, list_title)
+    local ok, snacks = pcall(require, 'snacks')
+    if ok and snacks.picker then
+        multi_select(icon_list, list_title, snacks)
+    else
+        single_select(icon_list, list_title)
+    end
 end
 
 fetcher.list = function()
